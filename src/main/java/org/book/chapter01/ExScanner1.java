c492ca1328ca9506d4ba2b9a1a698cf47ca29825
Kyeongrok <oceanfog1@gmail.com>
2023-01-07 20:40:32 +0900

    feat: Scanner examples - .nextInt() - .next() - .nextInt() 여러번

---
+++ src/main/java/org/book/chapter01/ExScanner1.java
@@ -0,0 +1,9 @@
+package org.book.chapter01;
+
+import java.util.Scanner;
+
+public class ExScanner1 {
+    public static void main(String[] args) {
+        Scanner sc;
+    }
+}
---
+++ src/main/java/org/book/chapter01/ExScanner3.java
@@ -0,0 +1,11 @@
+package org.book.chapter01;
+
+import java.util.Scanner;
+
+public class ExScanner3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+        System.out.println(sc.nextInt());
+    }
+}
