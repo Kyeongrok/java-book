fc25be93f557631ffab7bd3f3f63e6635e18c82c
Kyeongrok <oceanfog1@gmail.com>
2023-02-07 10:58:54 +0900

    ImmutableObject

---
+++ src/main/java/org/book/chapter03/p03/ImmutableObject.java
@@ -0,0 +1,9 @@
+package org.book.chapter03.p03;
+
+import org.book.chapter01.PrintHello;
+
+public class ImmutableObject {
+    public static void main(String[] args) {
+        final PrintHello printHello = new PrintHello();
+    }
+}
