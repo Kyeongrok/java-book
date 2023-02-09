61aaff425d07f00b6bee04ae9b4ce2703b11b2d0
Kyeongrok <oceanfog1@gmail.com>
2023-02-09 12:47:35 +0900

    SpaceInvaders

---
+++ src/main/java/org/book/chapter03/p04/SpaceInvaderMemberVariableTest.java
@@ -0,0 +1,10 @@
+package org.book.chapter03.p04;
+
+public class SpaceInvaderMemberVariableTest {
+    public static void main(String[] args) {
+        SpaceInvadersMemberVariable simv = new SpaceInvadersMemberVariable();
+        simv.moveLeft(); // 왼쪽으로 이동
+        simv.moveLeft(); // 왼쪽으로 이동
+        simv.moveRight(); // 오른쪽으로 이동
+    }
+}
---
+++ src/main/java/org/book/chapter03/p04/SpaceInvaders2Test.java
@@ -0,0 +1,9 @@
+package org.book.chapter03.p04;
+
+
+class SpaceInvaders2Test {
+    public static void main(String[] args) {
+        SpaceInvaders2 spaceInvaders2 = new SpaceInvaders2();
+    }
+
+}
