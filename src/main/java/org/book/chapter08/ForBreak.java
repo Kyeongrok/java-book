00e4b0f0466b9212ce7cdb11464ef86a98f31826
Kyeongrok Kim <oceanfog1@gmail.com>
2023-02-10 17:46:21 +0900

    ForBreak

---
+++ src/main/java/org/book/chapter08/ForBreak.java
@@ -0,0 +1,12 @@
+package org.book.chapter08;
+
+public class ForBreak {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
