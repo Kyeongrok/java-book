9c3ba91235df17faf405044b8b758ce6eed3da76
Kyeongrok Kim <oceanfog1@gmail.com>
2023-02-10 12:34:30 +0900

    SpaceInvaderMemberVariableTest

--- src/main/java/org/book/chapter03/p04/SpaceInvaderMemberVariableTest.java
+++ src/main/java/org/book/chapter03/p04/SpaceInvaderMemberVariableTest.java
@@ -3,8 +3,12 @@ package org.book.chapter03.p04;
 public class SpaceInvaderMemberVariableTest {
     public static void main(String[] args) {
         SpaceInvadersMemberVariable simv = new SpaceInvadersMemberVariable();
-        simv.moveLeft(); // 왼쪽으로 이동
-        simv.moveLeft(); // 왼쪽으로 이동
-        simv.moveRight(); // 오른쪽으로 이동
+        System.out.println(simv.location); // 멤버변수로 접근
+
+        simv.moveLeft(); // 왼쪽로 이동 -1
+        System.out.println(simv.location);
+
+        simv.moveLeft();
+        System.out.println(simv.location);
     }
 }
