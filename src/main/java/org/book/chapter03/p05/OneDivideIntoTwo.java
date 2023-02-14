ac502e877ceb7d13d91b4a7daf5e4d5d1f276659
Kyeongrok <oceanfog1@gmail.com>
2023-02-14 18:31:55 +0900

    OnePlusOne

---
+++ src/main/java/org/book/chapter03/p05/OneDivideIntoTwo.java
@@ -0,0 +1,7 @@
+package org.book.chapter03.p05;
+
+public class OneDivideIntoTwo {
+    public static void main(String[] args) {
+        System.out.println(1.0 / 2);
+    }
+}
---
+++ src/main/java/org/book/chapter03/p05/OnePlusOne.java
@@ -0,0 +1,7 @@
+package org.book.chapter03.p05;
+
+public class OnePlusOne {
+    public static void main(String[] args) {
+//        int result = 1 + "1";
+    }
+}
