b1cb077b29ee95b8638c5c9859554514be859f00
Kyeongrok <oceanfog1@gmail.com>
2023-02-14 20:34:21 +0900

    StringPlusOperation

---
+++ src/main/java/org/book/chapter03/p05/StringPlusOperation.java
@@ -0,0 +1,9 @@
+package org.book.chapter03.p05;
+
+public class StringPlusOperation {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String result = str1 + str2;
+    }
+}
