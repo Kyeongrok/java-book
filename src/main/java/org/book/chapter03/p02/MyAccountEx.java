d74c43e2b165284ae2bc1d74144e0d2b43347cf0
Kyeongrok <oceanfog1@gmail.com>
2023-02-14 21:06:25 +0900

    MyAccountEx

--- src/main/java/org/book/chapter03/p02/MyAccountEx.java
+++ src/main/java/org/book/chapter03/p02/MyAccountEx.java
@@ -3,6 +3,9 @@ package org.book.chapter03.p02;
 public class MyAccountEx {
     public static void main(String[] args) {
         int sumOfMyAccount = 1000;
-        System.out.printf("내 통장에는 %d원이 들어있습니다.", sumOfMyAccount);
+        System.out.printf("내 통장에는 %d원이 들어있습니다.\n", sumOfMyAccount);
+
+        sumOfMyAccount = 2000;
+        System.out.printf("내 통장에는 %d원이 들어있습니다.\n", sumOfMyAccount);
     }
 }
