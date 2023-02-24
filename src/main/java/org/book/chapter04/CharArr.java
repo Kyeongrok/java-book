05310d8bb14d4754075444915ced1a806415c283
Kyeongrok <oceanfog1@gmail.com>
2023-02-25 06:05:12 +0900

    ReferenceTypeArray

---
+++ src/main/java/org/book/chapter04/CharArr.java
@@ -0,0 +1,10 @@
+package org.book.chapter04;
+
+import java.util.Arrays;
+
+public class CharArr {
+    public static void main(String[] args) {
+        char[] cArr = new char[3];
+        System.out.println(Arrays.toString(cArr));
+    }
+}
---
+++ src/main/java/org/book/chapter04/Student.java
@@ -0,0 +1,7 @@
+package org.book.chapter04;
+
+public class Student {
+    String name;
+    String phoneNumber;
+    int age;
+}
