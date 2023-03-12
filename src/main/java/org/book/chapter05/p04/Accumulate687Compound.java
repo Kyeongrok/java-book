e270f80eca92bfe2bbf95d9980b30f02951ea98b
Kyeongrok <oceanfog1@gmail.com>
2023-03-12 11:41:27 +0900

    StringAccumulate

---
+++ src/main/java/org/book/chapter05/p04/Accumulate687Compound.java
@@ -0,0 +1,14 @@
+package org.book.chapter05.p04;
+
+public class Accumulate687Compound {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;
+        answer += num % 10;
+        num /= 10;
+        answer += num % 10;
+        num /= 10;
+        answer += num % 10;
+        System.out.printf("answer:%d\n", answer);
+    }
+}
