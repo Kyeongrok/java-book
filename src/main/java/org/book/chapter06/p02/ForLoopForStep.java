935f593f10a737edd8491ce96d9252de550a4cee
Kyeongrok <oceanfog1@gmail.com>
2023-03-28 11:17:29 +0900

    ForLoopStep

---
+++ src/main/java/org/book/chapter06/p02/ForLoopForStep.java
@@ -0,0 +1,9 @@
+package org.book.chapter06.p02;
+
+public class ForLoopForStep {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
