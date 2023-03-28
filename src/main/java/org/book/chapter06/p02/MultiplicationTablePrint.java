a055d6923fc45dbb4f5c7611d3feeedfc332b7d6
Kyeongrok <oceanfog1@gmail.com>
2023-03-28 22:38:53 +0900

    MultiplicationTableStep4

---
+++ src/main/java/org/book/chapter06/p02/MultiplicationTablePrint.java
@@ -0,0 +1,9 @@
+package org.book.chapter06.p02;
+
+public class MultiplicationTablePrint {
+    public static void main(String[] args) {
+        System.out.printf("2 * 1 = 2\n");
+        System.out.printf("2 * 2 = 4\n");
+        System.out.printf("2 * 3 = 6\n");
+    }
+}
---
+++ src/main/java/org/book/chapter06/p02/MultiplicationTableStep2.java
@@ -0,0 +1,9 @@
+package org.book.chapter06.p02;
+
+public class MultiplicationTableStep2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n",2, i, 2 * i);
+        }
+    }
+}
