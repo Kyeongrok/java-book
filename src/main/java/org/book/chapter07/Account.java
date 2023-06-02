a219a7325ad53febaf72eb2c3bae844e41df7cbd
Kyeongrok <oceanfog1@gmail.com>
2023-06-02 15:16:50 +0900

    Payment

--- src/main/java/org/book/chapter07/Account.java
+++ src/main/java/org/book/chapter07/Account.java
@@ -1,9 +1,8 @@
 package org.book.chapter07;
 
 public class Account {
-    int balance = 1_000_000;
-
-    public void printBalanceWithName(String name) {
-
+    int balance = 2000;
+    public boolean isSufficient(){ // 리턴타입, 메소드명 변경
+        return balance >= 1500;
     }
 }
