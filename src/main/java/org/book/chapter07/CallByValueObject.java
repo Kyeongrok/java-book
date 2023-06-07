e9be42e3f74c828b1dd1699a054eb6b4ba292a8a
Kyeongrok Kim <oceanfog1@gmail.com>
2023-06-07 16:55:11 +0900

    CallByValue

---
+++ src/main/java/org/book/chapter07/CallByValueObject.java
@@ -0,0 +1,18 @@
+package org.book.chapter07;
+
+public class CallByValueObject {
+
+    public User callByValueObjectTest(User value){
+        value.age += 100;
+        return value;
+    }
+
+    public static void main(String[] args) {
+        CallByValueObject cbv = new CallByValueObject();
+        User user1 = new User();
+        user1.age = 37;
+        User result = cbv.callByValueObjectTest(user1);
+        System.out.println("result.age = " + result.age);
+        System.out.println("user1.age = " + user1.age);
+    }
+}
