6876509b0b52b78b97ca7ca44a1b8406cd0fe750
kyeongrok.kim <kyeongrok.kim@e3view.com>
2024-03-17 13:51:25 +0900

    PrintBye

---
+++ src/main/java/org/book/chapter01/question/PrintBye.java
@@ -0,0 +1,12 @@
+package org.book.chapter01.question;
+
+public class PrintBye {
+    public void print(){
+        System.out.println("Bye");
+    }
+
+    public static void main(String[] args) {
+        PrintBye printBye = new PrintBye();
+        printBye.print();
+    }
+}
